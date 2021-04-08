bf63d920353190a5cce89ce5d9af03c959e340f6
taotaowang-study <864734049@qq.com>
2021-04-08 20:15:24 +0800

    提交

--- src/main/java/com/study/rabbitmq/RabbitmqApplication.java
+++ src/main/java/com/study/rabbitmq/RabbitmqApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RabbitmqApplication {
 
 
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(RabbitmqApplication.class, args);
     }
