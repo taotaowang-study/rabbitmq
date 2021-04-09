6c7bb93d3e864655010eb1c249f541d047e179da
XimenGuanren <allan864734049@dingtalk.com>
2021-04-09 20:54:00 +0800

    DASDAS

---
+++ src/main/java/com/study/rabbitmq/provider/Producer.java
@@ -0,0 +1,36 @@
+package com.study.rabbitmq.provider;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Producer {
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        // 创建连接
+        ConnectionFactory factory = new ConnectionFactory();
+        // 设置 RabbitMQ 的主机名
+        factory.setHost("47.99.107.252");
+        factory.setPort(5672);
+        factory.setUsername("developer");
+        factory.setPassword("developer");
+        factory.setVirtualHost("/");
+        // 创建一个连接
+        Connection connection = factory.newConnection();
+        // 创建一个通道
+        Channel channel = connection.createChannel();
+        // 指定一个队列,不存在的话自动创建
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        // 发送消息
+        String message = "Hello World!";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+        System.out.println(" [x] Sent '" + message + "'");
+        // 关闭频道和连接
+        channel.close();
+        connection.close();
+    }
+}
